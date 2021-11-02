d4c77b19314fd1b796ad2c0ffcd3b7de62da7240
aymak91 <amakyu@gmail.com>
2021-11-02 15:25:46 -0700

    peak element

---
+++ 0162_find_peak_element.js
@@ -0,0 +1,60 @@
+// 162. Find Peak Element
+// Medium
+
+// A peak element is an element that is strictly greater than its neighbors.
+
+// Given an integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+// You may imagine that nums[-1] = nums[n] = -∞.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: 2
+// Explanation: 3 is a peak element and your function should return the index number 2.
+// Example 2:
+
+// Input: nums = [1,2,1,3,5,6,4]
+// Output: 5
+// Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 1000
+// -231 <= nums[i] <= 231 - 1
+// nums[i] != nums[i + 1] for all valid i.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ const findPeakElementIterative = function(nums) {
+    
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left < right) {
+        let mid = Math.floor(left + (right - left) / 2)
+        
+        if (nums[mid] < nums[mid+1]) {
+            left = mid+1;
+        } else {
+            right = mid;
+        }
+    }
+    return left;
+};
+
+var findPeakElementRecursive = function(nums) {
+    return search(nums, 0, nums.length-1);
+};
+
+let search = function(nums, l, r) {
+    if (l === r) return l;
+    let mid = Math.floor(l+(r-l)/2);
+    if (nums[mid] > nums[mid+1]) return search(nums, l, mid);
+    return search (nums, mid+1, r);
+}
