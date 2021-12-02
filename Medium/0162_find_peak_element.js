96ff047c63e1cfdcf0201165da21e0e7781cba61
aymak91 <amakyu@gmail.com>
2021-12-02 15:41:42 -0800

    cleanup peak element

--- Medium/0162_find_peak_element.js
+++ Medium/0162_find_peak_element.js
@@ -39,20 +39,17 @@
     while (left < right) {
         let mid = Math.floor(left + (right - left) / 2)
         
-        if (nums[mid] < nums[mid+1]) {
-            left = mid+1;
-        } else {
-            right = mid;
-        }
+        if (nums[mid] < nums[mid+1]) left = mid+1;
+        if (nums[mid] >= nums[mid+1]) right = mid;
     }
     return left;
 };
 
-var findPeakElementRecursive = function(nums) {
+const findPeakElementRecursive = function(nums) {
     return search(nums, 0, nums.length-1);
 };
 
-let search = function(nums, l, r) {
+const search = function(nums, l, r) {
     if (l === r) return l;
     let mid = Math.floor(l+(r-l)/2);
     if (nums[mid] > nums[mid+1]) return search(nums, l, mid);
