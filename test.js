96fdc23f9b18e91ecf18134e8d94cff533c64204
aymak91 <amakyu@gmail.com>
2022-09-02 16:06:28 -0700

    koko eating bananas

--- test.js
+++ test.js
@@ -1,35 +1,47 @@
-function chooseFlask(requirements, flaskTypes, markings) {
-    requirements = requirements.sort((a, b) => a - b)
-    let minWaste = {
-        sum: Infinity,
-        flaskNum: -1
-    }
-    
-    let i = 0
-    let j = 0
-    
-    while (i < flaskTypes) {
-        let currWasteSum = 0
-        let reqNum = 0
-        let currFlaskNum = i
-        
-        while (reqNum < requirements.length && j < markings.length && markings[j][0] === currFlaskNum) {
-            if (requirements[reqNum] > markings[j][1]) {
-                j++
+const lastRow = maze.length - 1;
+const lastCol = maze[0].length - 1;
+
+const DIRECTIONS = [[0,1],[0,-1],[1,0],[-1,0]];
+
+const queue = [];
+const addToQueue = (row,col,step) => {
+    maze[row][col] = -1;
+    queue.push([row,col,step]);
+}
+
+//add start location to queue
+addToQueue(0,0,0);
+
+const isBetween = (num, lower, upper) => lower <= num && num <= upper;
+const validUnvisitedCell = (row,col) => {
+    return isBetween(row, 0, lastRow) && isBetween(col, 0, lastCol) && maze[row][col] === 0;
+}
+
+
+while(queue.length){
+    const [row,col,step] = queue.shift();
+    if(row === lastRow && col === lastCol) return step;
+    const newStep = step + 1;
+    let dirSkipFlag = 0;
+    for(let jumpSize=1; jumpSize<=k; jumpSize++){
+        for(let i =0; i<4; i++){
+            const dirFlag = 1<<i;
+            if(dirSkipFlag & dirFlag) continue;
+            const [dr,dc] = DIRECTIONS[i];
+            let nRow = row + dr*jumpSize;
+            let nCol = col + dc*jumpSize;
+            if(validUnvisitedCell(nRow,nCol)){
+                addToQueue(nRow,nCol,newStep);
             } else {
-                currWasteSum += markings[j][1] - requirements[reqNum]
-                reqNum++
+                dirSkipFlag |= dirFlag;
             }
         }
-        i++
-        while (j < markings.length && markings[j][0] === currFlaskNum) { j++ }
-        if (reqNum !== requirements.length) continue
-
-        if (currWasteSum < minWaste.sum) {
-            minWaste.sum = currWasteSum
-            minWaste.flaskNum = currFlaskNum
-        }
     }
-
-    return minWaste.flaskNum
 }
+return -1;
+
+// 0 0000000000000000
+// 1 0000000000000001
+// 2 0000000000000010
+// 3 0000000000000011
+
