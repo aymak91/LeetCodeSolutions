7daf90be08024ce4b3d23acf0365a9c72318f8e7
aymak91 <amakyu@gmail.com>
2022-10-24 02:35:07 -0700

    abstract design browser history

--- Medium/1472_design_browser_history.js
+++ Medium/1472_design_browser_history.js
@@ -1,75 +1,30 @@
-// 1472. Design Browser History
-// Medium
-// You have a browser of one tab where you start on the homepage and you can visit another url, get back in the history number of steps or move forward in the history number of steps.
-
-// Implement the BrowserHistory class:
-
-// BrowserHistory(string homepage) Initializes the object with the homepage of the browser.
-// void visit(string url) Visits url from the current page. It clears up all the forward history.
-// string back(int steps) Move steps back in history. If you can only return x steps in the history and steps > x, you will return only x steps. Return the current url after moving back in history at most steps.
-// string forward(int steps) Move steps forward in history. If you can only forward x steps in the history and steps > x, you will forward only x steps. Return the current url after forwarding in history at most steps.
- 
-
-// Example:
-
-// Input:
-// ["BrowserHistory","visit","visit","visit","back","back","forward","visit","forward","back","back"]
-// [["leetcode.com"],["google.com"],["facebook.com"],["youtube.com"],[1],[1],[1],["linkedin.com"],[2],[2],[7]]
-// Output:
-// [null,null,null,null,"facebook.com","google.com","facebook.com",null,"linkedin.com","google.com","leetcode.com"]
-
-// Explanation:
-// BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
-// browserHistory.visit("google.com");       // You are in "leetcode.com". Visit "google.com"
-// browserHistory.visit("facebook.com");     // You are in "google.com". Visit "facebook.com"
-// browserHistory.visit("youtube.com");      // You are in "facebook.com". Visit "youtube.com"
-// browserHistory.back(1);                   // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
-// browserHistory.back(1);                   // You are in "facebook.com", move back to "google.com" return "google.com"
-// browserHistory.forward(1);                // You are in "google.com", move forward to "facebook.com" return "facebook.com"
-// browserHistory.visit("linkedin.com");     // You are in "facebook.com". Visit "linkedin.com"
-// browserHistory.forward(2);                // You are in "linkedin.com", you cannot move forward any steps.
-// browserHistory.back(2);                   // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
-// browserHistory.back(7);                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
- 
-
-// Constraints:
-// 1 <= homepage.length <= 20
-// 1 <= url.length <= 20
-// 1 <= steps <= 100
-// homepage and url consist of  '.' or lower case English letters.
-// At most 5000 calls will be made to visit, back, and forward.
-
 class BrowserHistory {
     constructor(homepage) {
         this.browserHistory = new DoublyLinkedList(homepage);
-        this.current = this.browserHistory.head.next;
     }
 
     visit(url) {
         const newURL = new Node(url);
-        newURL.next = this.browserHistory.tail;
-        newURL.prev = this.current;
-        this.current.next = newURL;
-        this.current = newURL;
+        this.browserHistory.add(newURL);
     }
 
     back(steps) {
 
-        while (steps > 0 && this.current.prev.val !== null) {
-            this.current = this.current.prev;
+        while (steps > 0 && this.browserHistory.current.prev.val !== null) {
+            this.browserHistory.current = this.browserHistory.current.prev;
             steps--;
         }
         
-        return this.current.val;
+        return this.browserHistory.current.val;
     }
 
     forward(steps) {
-        while (steps > 0 && this.current.next.val !== null) {
-            this.current = this.current.next;
+        while (steps > 0 && this.browserHistory.current.next.val !== null) {
+            this.browserHistory.current = this.browserHistory.current.next;
             steps--;
         }
         
-        return this.current.val;
+        return this.browserHistory.current.val;
     }
 }
 
@@ -92,6 +47,15 @@ class DoublyLinkedList {
 
         this.homepage.prev = this.head;
         this.homepage.next = this.tail;
+
+        this.current = this.homepage;
+    }
+
+    add(node) {
+        node.next = this.tail;
+        node.prev = this.current;
+        this.current.next = node;
+        this.current = node;
     }
 }
 
