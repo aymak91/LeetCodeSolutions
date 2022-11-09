f9347e92720c9106a28dde8506c2772c35902ba2
aymak91 <amakyu@gmail.com>
2022-11-09 10:52:41 -0800

    rotting oranges

--- Medium/0994_rotting_oranges.js
+++ Medium/0994_rotting_oranges.js
@@ -30,3 +30,124 @@
 // n == grid[i].length
 // 1 <= m, n <= 10
 // grid[i][j] is 0, 1, or 2.
+
+var orangesRotting = function(grid) {
+    
+
+    let queue = new DoublyLinkedList();
+    let freshOranges = 0;
+    let maxTime = 0;
+    
+    for (let row=0; row<grid.length; row++) {
+        for (let col=0; col<grid[0].length; col++) {
+            if (grid[row][col] === 1) freshOranges++;
+            if (grid[row][col] === 2) queue.add(new Node([row, col, 0]))
+        }
+    }
+    
+    while (queue.length > 0) {
+        const [row, col, level] = queue.pop();
+        if (level > maxTime) maxTime = level;
+        
+        const directions = [[row+1, col],[row-1, col],[row, col+1],[row, col-1]];
+        for (let dir of directions) {
+
+            if (validCoordinate(grid, dir[0], dir[1]) && grid[dir[0]][dir[1]] === 1) {
+                queue.add(new Node([dir[0], dir[1], level+1]));
+                grid[dir[0]][dir[1]] = 2;
+                freshOranges--;            
+            }
+
+        }
+    }
+    
+    return freshOranges === 0 ? maxTime : -1;
+};
+
+
+
+// array solution
+const validCoordinate = function(board, row, col) {
+    const validRow = row >= 0 && row < board.length;
+    const validCol = col >= 0 && col <board[0].length;
+    return validRow && validCol;
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = new Node(null);
+        this.tail = new Node(null);
+        
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+        this.length = 0;
+    }
+    
+    add(node) {
+        let prev = this.head;
+        let nxt = this.head.next;
+        
+        prev.next = node;
+        nxt.prev = node;
+        
+        node.prev = prev;
+        node.next = nxt;
+        
+        this.length++;
+    }
+    
+    pop() {
+        if (this.length === 0) return;
+        
+        const node = this.tail.prev;
+        let prev = node.prev;
+        let nxt = this.tail;
+        // console.log(node, prev)
+        prev.next = nxt;
+        nxt.prev = prev;
+        this.length--;
+        
+        return node.val;
+    }
+}
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+var orangesRotting = function(grid) {
+    
+
+    let queue = [];
+    let freshOranges = 0;
+    let maxTime = 0;
+    
+    for (let row=0; row<grid.length; row++) {
+        for (let col=0; col<grid[0].length; col++) {
+            if (grid[row][col] === 1) freshOranges++;
+            if (grid[row][col] === 2) queue.push([row, col, 0])
+        }
+    }
+    
+    while (queue.length > 0) {
+        const [row, col, level] = queue.shift();
+        if (level > maxTime) maxTime = level;
+        
+        const directions = [[row+1, col],[row-1, col],[row, col+1],[row, col-1]];
+        for (let dir of directions) {
+
+            if (validCoordinate(grid, dir[0], dir[1]) && grid[dir[0]][dir[1]] === 1) {
+                queue.push([dir[0], dir[1], level+1]);
+                grid[dir[0]][dir[1]] = 2;
+                freshOranges--;            
+            }
+
+        }
+    }
+    
+    return freshOranges === 0 ? maxTime : -1;
+};
