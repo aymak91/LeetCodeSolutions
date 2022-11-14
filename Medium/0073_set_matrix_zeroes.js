6e7cd359c406eb196418646dd1c542b67c25218e
aymak91 <amakyu@gmail.com>
2022-11-14 12:58:06 -0800

    set matrix zeroes

---
+++ Medium/0073_set_matrix_zeroes.js
@@ -0,0 +1,12 @@
+// 73. Set Matrix Zeroes
+// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+// You must do it in place.
+
+// Example 1:
+// Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+// Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+// Example 2:
+// Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+// Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
