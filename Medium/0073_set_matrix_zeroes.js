d1ae7a032e5e6bf8825c3e9935bf61f44ce27700
aymak91 <amakyu@gmail.com>
2022-11-14 13:33:36 -0800

    set matrix zeroes

--- Medium/0073_set_matrix_zeroes.js
+++ Medium/0073_set_matrix_zeroes.js
@@ -10,3 +10,31 @@
 // Example 2:
 // Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 // Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+ let setZeroes = function(matrix) {
+   
+    const rows = new Set();
+    const cols = new Set();
+    
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j] === 0) {
+                rows.add(i);
+                cols.add(j);
+            }
+        }
+    }
+    
+    
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (rows.has(i) || cols.has(j)) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+};
